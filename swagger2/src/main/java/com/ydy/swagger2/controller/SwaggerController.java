caf40dca4064c063cffb5919137ef050880685e2
mikeyangdy <942091077@qq.com>
2020-08-27 08:56:04 +0800

    swagger2增加restTemplate测试及配置configuration

--- swagger2/src/main/java/com/ydy/swagger2/controller/SwaggerController.java
+++ swagger2/src/main/java/com/ydy/swagger2/controller/SwaggerController.java
@@ -1,18 +1,26 @@
 package com.ydy.swagger2.controller;
 
 import com.alibaba.fastjson.JSONObject;
-import com.ydy.swagger2.entity.Stu;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
 
 @RestController
 @Api(tags = "swagger测试API")
 public class SwaggerController {
 
+    @Resource
+    private RestTemplate restTemplate;
 
+    @GetMapping("/restTemplateT")
+    public String restTemplateT(){
+        return restTemplate.getForObject("http://121.199.2.1/getJson",String.class);
+    }
 
     @GetMapping("/getJson")
     @ApiOperation(value = "获取测试json数据",notes = "哈哈哈")
